e50bfc619e0a24086d16c04b906231bb113a1b38
Matt Turner <matt@fiverings.uk>
2020-04-27 19:00:10 +0100

    Open/close on move hand

--- treasure-box.ino
+++ treasure-box.ino
@@ -2,20 +2,33 @@
 #include <HCSR04.h>
 
 Servo servoLid;
-UltraSonicDistanceSensor sensorOpen (12, 11);
+UltraSonicDistanceSensor sensorOpen (11, 10);
+bool isOpen;
 
 void setup() {
   Serial.begin(9600);
-  servoLid.attach(10);
+  servoLid.attach(12);
   servoLid.write(90);
+  isOpen = false;
 }
 
 void loop() {
   double distance = sensorOpen.measureDistanceCm();
+  if (distance == -1) {
+    return;
+  }
+  
+  Serial.print("Distance: ");
+  Serial.println(distance);
 
-  if (distance < 10) { 
-    servoLid.write(0);
-    delay(10000);
-    servoLid.write(90);
+  if (distance < 10) {    
+    if (isOpen) {
+      servoLid.write(90);
+    } else {
+      servoLid.write(0);
+    }
+
+    isOpen = !isOpen;
+    delay(500);
   }
 }
