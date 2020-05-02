4355f194814b1e01c3349c6e42596af6c3933fd2
Matt Turner <matt@fiverings.uk>
2020-05-02 15:21:31 +0100

    Add lock mechanism

--- treasure-box.ino
+++ treasure-box.ino
@@ -2,6 +2,7 @@
 #include <HCSR04.h>
 
 Servo servoLid;
+Servo servoLatch;
 UltraSonicDistanceSensor sensorOpen (11, 10);
 bool isOpen;
 
@@ -9,7 +10,10 @@ void setup() {
   Serial.begin(57600);
   Serial.println("Starting up...");
   servoLid.attach(12);
+  servoLatch.attach(9);
   closeLid();
+  lock();
+  sensorOpen.measureDistanceCm();
 }
 
 void loop() {
@@ -22,14 +26,16 @@ void loop() {
   Serial.print("Distance: ");
   Serial.println(distance);
 
-  if (distance > 10) {
+  if (distance > 5) {
     delay(500);
     return;
   }
       
   if (isOpen) {
     closeLid();
+    lock();
   } else {
+    unlock();
     openLid();
   }
 
@@ -39,11 +45,29 @@ void loop() {
 void openLid() {
   Serial.println("Opening lid...");
   servoLid.write(0);
+  awaitReposition();
   isOpen = true;
 }
 
 void closeLid() {
   Serial.println("Closing lid...");
   servoLid.write(107);
+  awaitReposition();
   isOpen = false;
+}
+
+void lock() {
+  Serial.println("Locking lid...");
+  servoLatch.write(140);
+  awaitReposition();
+}
+
+void unlock() {
+  Serial.println("Unlocking lid...");
+  servoLatch.write(70);
+  awaitReposition();
+}
+
+void awaitReposition() {
+  delay(300);
 }
