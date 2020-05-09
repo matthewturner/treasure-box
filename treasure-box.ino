29473158c9cedfb1c311b85ff7225dbbc1fb74ff
Matt Turner <matt@fiverings.uk>
2020-05-09 16:00:43 +0100

    Complete rfid support

--- treasure-box.ino
+++ treasure-box.ino
@@ -12,23 +12,27 @@ void setup() {
   Serial.println("Starting up...");
   SPI.begin();
   sensorOpen.PCD_Init();
-  delay(4);
+  delay(10);
   sensorOpen.PCD_DumpVersionToSerial();
   Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
   
-  servoLid.attach(5);
-  servoLatch.attach(6);
+  servoLid.attach(7);
+  servoLatch.attach(8);
   closeLid();
   lock();
+  // unlock();
+  // openLid();
 }
 
 void loop() {
   if (!sensorOpen.PICC_IsNewCardPresent()) {
+    // Serial.println("No card present");
     delay(500);
     return;
   }
 
   if (!sensorOpen.PICC_ReadCardSerial()) {
+    Serial.println("Card read failed");
     delay(500);
     return;
   }
@@ -37,8 +41,8 @@ void loop() {
   String uuid = readUUID();
   Serial.println(uuid);
   
-  if (uuid != "D4 37 43 73")
-  {
+  if (uuid != "D4 37 43 73") { // card
+  // if (uuid != "C0 4B 19 A4") { // fob
     Serial.println("Access denied!");
     delay(500);
     return;
@@ -71,18 +75,6 @@ void closeLid() {
   isOpen = false;
 }
 
-String readUUID() {
-  String content= "";
-  byte letter;
-  for (byte i = 0; i < sensorOpen.uid.size; i++) 
-  {
-     content.concat(String(sensorOpen.uid.uidByte[i] < 0x10 ? " 0" : " "));
-     content.concat(String(sensorOpen.uid.uidByte[i], HEX));
-  }
-  content.toUpperCase();
-  return content.substring(1);
-}
-
 void lock() {
   Serial.println("Locking lid...");
   servoLatch.write(140);
@@ -97,4 +89,16 @@ void unlock() {
 
 void awaitReposition() {
   delay(300);
+}
+
+String readUUID() {
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < sensorOpen.uid.size; i++) 
+  {
+     content.concat(String(sensorOpen.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(sensorOpen.uid.uidByte[i], HEX));
+  }
+  content.toUpperCase();
+  return content.substring(1);
 }
